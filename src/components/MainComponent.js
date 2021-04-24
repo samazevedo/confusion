02d0078a652b97664c5fbe93844749e5a629da57
sam-azevedo <samuel.azevedo@live.com>
2021-04-24 15:36:25 -0300

    controlled forms

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Home from './HomeComponent';
 import DishDetail from './DishdetailComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { LEADERS } from '../shared/leaders';
@@ -23,7 +24,6 @@ class Main extends Component {
             selectedDish: null
         };
     }
-
     render() {
         const HomePage = () => {
             return (
@@ -40,7 +40,6 @@ class Main extends Component {
                     comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}
                 />
             );
-
         }
         return (
             <div className="MainPage">
@@ -50,6 +49,7 @@ class Main extends Component {
                     <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
                     <Route path="/menu/:dishId" component={DishWithId} />
                     <Route exact path="/contactus" component={Contact} />
+                    <Route exact path="/aboutus" component={() => <About leaders={this.state.leaders} />} />
                     <Redirect to="/home" />
                 </Switch>
                 <Footer />
